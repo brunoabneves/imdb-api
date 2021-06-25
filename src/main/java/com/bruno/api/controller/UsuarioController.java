0c4f7c452a2845eda3cc6f75ec2262961b87c48f
Bruno <brunoabneves97@gmail.com>
2021-06-25 17:47:12 -0300

    Implementando Collection Resource

--- src/main/java/com/bruno/api/controller/UsuarioController.java
+++ src/main/java/com/bruno/api/controller/UsuarioController.java
@@ -1,14 +1,32 @@
 package com.bruno.api.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bruno.domain.model.TipoUsuario;
+import com.bruno.domain.model.Usuario;
 
 @RestController
 public class UsuarioController {
 	
 	@GetMapping("/usuarios")
-	public String Listar () {
-		return "Testando";
+	public List<Usuario> listar () {
+		var usuario1 = new Usuario();
+		usuario1.setId(1L);
+		usuario1.setNome("João Das Neves");
+		usuario1.setEmail("joaodasneves@email.com");
+		usuario1.setTipo(TipoUsuario.COMUM);
+		
+		var usuario2 = new Usuario();
+		usuario2.setId(2L);
+		usuario2.setNome("Anakin Skywalker");
+		usuario2.setEmail("anakin@email.com");
+		usuario2.setTipo(TipoUsuario.ADMINISTRADOR);
+		
+		return Arrays.asList(usuario1, usuario2);
 	}
 
 }
