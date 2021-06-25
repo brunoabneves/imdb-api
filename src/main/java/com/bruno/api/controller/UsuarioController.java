c9238f352af341a60da51a0954caaac7d56eb11b
Bruno <brunoabneves97@gmail.com>
2021-06-25 20:00:20 -0300

    Usando Spring Data JPA

--- src/main/java/com/bruno/api/controller/UsuarioController.java
+++ src/main/java/com/bruno/api/controller/UsuarioController.java
@@ -2,24 +2,23 @@ package com.bruno.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bruno.domain.model.Usuario;
+import com.bruno.domain.repository.UsuarioRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class UsuarioController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	private UsuarioRepository usuarioRepository;
 	
 	@GetMapping("/usuarios")
 	public List<Usuario> listar() {
-		return manager.createQuery("from Usuario", Usuario.class)
-				.getResultList();
+		return usuarioRepository.findAll();
 	}
 
 }
---
+++ src/main/java/com/bruno/domain/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.bruno.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bruno.domain.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+}
