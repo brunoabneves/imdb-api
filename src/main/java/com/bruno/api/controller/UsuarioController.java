f1562e180a531fc28cd87d5060103685ddb7da6b
Bruno <brunoabneves97@gmail.com>
2021-06-25 20:24:03 -0300

    Implementando o CRUD de Usuario

--- src/main/java/com/bruno/api/controller/UsuarioController.java
+++ src/main/java/com/bruno/api/controller/UsuarioController.java
@@ -2,7 +2,16 @@ package com.bruno.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bruno.domain.model.Usuario;
@@ -12,13 +21,44 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 	
 	private UsuarioRepository usuarioRepository;
 	
-	@GetMapping("/usuarios")
+	@GetMapping
 	public List<Usuario> listar() {
 		return usuarioRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Usuario cadastrar (@RequestBody Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+	
+	@PutMapping("/{usuarioId}")
+	public ResponseEntity<Usuario> editar (@PathVariable Long usuarioId, @RequestBody Usuario usuario) {
+		if(!usuarioRepository.existsById(usuarioId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		usuario.setId(usuarioId);
+		usuario = usuarioRepository.save(usuario);
+		
+		return ResponseEntity.ok(usuario);
+	}
+	
+	@DeleteMapping("/{usuarioId}")
+	public ResponseEntity<Void> deletar(@PathVariable Long usuarioId) {
+
+		if (!usuarioRepository.existsById(usuarioId)) {
+			return ResponseEntity.notFound().build();
+		}
+
+		usuarioRepository.deleteById(usuarioId);
+
+		return ResponseEntity.noContent().build();
 	}
 
 }
