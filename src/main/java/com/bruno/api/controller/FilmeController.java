3ea4e33b14910bfce0b56b828980b869cb6dfbfe
Bruno <brunoabneves97@gmail.com>
2021-06-28 21:27:02 -0300

    Isolando Domain Model do Representation Model

--- src/main/java/com/bruno/api/controller/FilmeController.java
+++ src/main/java/com/bruno/api/controller/FilmeController.java
@@ -6,16 +6,15 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bruno.api.model.FilmeModel;
 import com.bruno.domain.model.Filme;
 import com.bruno.domain.repository.FilmeRepository;
 import com.bruno.domain.service.CrudFilmeService;
@@ -36,18 +35,45 @@ public class FilmeController {
 	}
 	
 	@GetMapping("/genero/{genero}")
-	public List<Filme> listarPorGenero(@PathVariable String genero) {
-		return filmeRepository.findByGenero(genero);
+	public ResponseEntity<FilmeModel> listarPorGenero(@PathVariable String genero) {
+		return filmeRepository.findByGenero(genero)
+				.map(filme ->{
+					FilmeModel filmeModel = new FilmeModel();
+					filmeModel.setId(filme.getId());
+					filmeModel.setNome(filme.getNome());
+					filmeModel.setDiretor(filme.getDiretor());
+					filmeModel.setGenero(filme.getGenero());
+					
+					return ResponseEntity.ok(filmeModel);
+				}).orElse(ResponseEntity.notFound().build());
 	}
 	
 	@GetMapping("/nome/{nome}")
-	public List<Filme> listarPorNome(@PathVariable String nome) {
-		return filmeRepository.findByNomeContaining(nome);
+	public ResponseEntity<FilmeModel> listarPorNome(@PathVariable String nome) {
+		return filmeRepository.findByNomeContaining(nome)
+				.map(filme ->{
+					FilmeModel filmeModel = new FilmeModel();
+					filmeModel.setId(filme.getId());
+					filmeModel.setNome(filme.getNome());
+					filmeModel.setDiretor(filme.getDiretor());
+					filmeModel.setGenero(filme.getGenero());
+					
+					return ResponseEntity.ok(filmeModel);
+				}).orElse(ResponseEntity.notFound().build());
 	}
 	
 	@GetMapping("/diretor/{diretor}")
-	public List<Filme> listarPorDiretor(@PathVariable String diretor) {
-		return filmeRepository.findByDiretorContaining(diretor);
+	public ResponseEntity<FilmeModel> listarPorDiretor(@PathVariable String diretor) {
+		return filmeRepository.findByDiretorContaining(diretor)
+				.map(filme ->{
+					FilmeModel filmeModel = new FilmeModel();
+					filmeModel.setId(filme.getId());
+					filmeModel.setNome(filme.getNome());
+					filmeModel.setDiretor(filme.getDiretor());
+					filmeModel.setGenero(filme.getGenero());
+					
+					return ResponseEntity.ok(filmeModel);
+				}).orElse(ResponseEntity.notFound().build());
 	}
 	
 	/*@GetMapping("/ator/{ator}")
@@ -59,29 +85,5 @@ public class FilmeController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Filme cadastrar (@Valid @RequestBody Filme filme) {
 		return crudFilmeService.salvar(filme);
-	}
-	
-	@PutMapping("/{filmeId}")
-	public ResponseEntity<Filme> editar (@Valid @PathVariable Long filmeId, @RequestBody Filme filme) {
-		if(!filmeRepository.existsById(filmeId)) {
-			return ResponseEntity.notFound().build();
-		}
-		
-		filme.setId(filmeId);
-		filme = crudFilmeService.salvar(filme);
-		
-		return ResponseEntity.ok(filme);
-	}
-	
-	@DeleteMapping("/{filmeId}")
-	public ResponseEntity<Void> deletar(@PathVariable Long filmeId) {
-
-		if (!filmeRepository.existsById(filmeId)) {
-			return ResponseEntity.notFound().build();
-		}
-
-		crudFilmeService.excluir(filmeId);
-
-		return ResponseEntity.noContent().build();
 	}
 }
---
+++ src/main/java/com/bruno/api/model/FilmeModel.java
@@ -0,0 +1,16 @@
+package com.bruno.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FilmeModel {
+
+	private Long id;
+	private String nome;
+	//private String nomeAtor;
+	private String genero;
+	private String diretor;
+	
+}
